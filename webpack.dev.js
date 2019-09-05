aedccd4d6d3729e8f5d257d8479638649e0523bc
rj1809 <rahuljindal1989@gmail.com>
2019-09-05 20:31:08 +0530

    init

---
+++ webpack.dev.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const common = require("./webpack.common")
+const merge = require("webpack-merge")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common,{
+    mode : "development",
+    output : {
+        filename : "[name].bundle.js",
+        path : path.resolve(__dirname,"dist")
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // Inject styles into DOM 
+                    "css-loader", //2. Turns css into commonjs
+                    "sass-loader" //1. Turns scss into css
+                ]
+            }
+        ]
+    }
+})
